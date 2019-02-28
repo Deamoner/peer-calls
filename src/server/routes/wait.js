301806c279d3b7c783e621fc92e1a852036ac89f
Matt Davis <matt@healthbrain.co>
2019-02-28 14:57:30 -0500

    Fix for auto associate

--- src/server/routes/wait.js
+++ src/server/routes/wait.js
@@ -8,15 +8,20 @@ const uuid = require('uuid')
 const BASE_URL = config.get('baseUrl')
 
 router.get('/', (req, res) => {
+  var toggle = false;
   console.log(ioServer.sockets.adapter.rooms);
   const counter = Object.keys(ioServer.sockets.adapter.rooms)
   const shufrooms = shuffle(counter)
   shufrooms.forEach(function(room) {
     if(room.length > 30) {
       res.redirect(`${BASE_URL}/call/${room}`)
+      toggle = true;
     }
   })
-  res.redirect(`${BASE_URL}/call/${uuid.v4()}`)
+  if(toggle == false ){
+    res.redirect(`${BASE_URL}/call/${uuid.v4()}`)
+  }
+  
 })
 
 
